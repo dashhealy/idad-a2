ae9b620216354ce1efd612b6ae4580cab3653ab9
dashhealy <S4129466@student.rmit.edu.au>
2025-09-11 23:47:11 +1000

    more stuff

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function checkTime() {
   let seconds = systemDate.getSeconds();
 
   let month = systemDate.getMonth();
-  month = month + 1;
+  //month = month + 1;
+  month = 6;
 
   timeUI(month, hours, minutes, seconds);
 }
@@ -22,25 +23,33 @@ function checkTime() {
 function timeUI(month, hours, minutes, seconds) {
   if (month === 12 || month === 1 || month === 2) {
     let season = "Summer";
-    document.documentElement.style.setProperty("--col02", "#f3ebabff");
+    document.documentElement.style.setProperty("--col01", "#eaca63");
+    document.documentElement.style.setProperty("--col02", "#f3ebab");
+    document.documentElement.style.setProperty("--col03", "#ffcc66");
     hoursDiplay.src = `assets/${season}${hours}.png`;
     minutesDiplay.src = `assets/${season}${minutes}.png`;
     secondsDiplay.src = `assets/${season}${seconds}.png`;
   } else if (month === 3 || month === 4 || month === 5) {
     let season = "autumn";
-    document.documentElement.style.setProperty("--col02", "#f4cc90ff");
+    document.documentElement.style.setProperty("--col01", "#e89f49");
+    document.documentElement.style.setProperty("--col02", "#f4cc90");
+    document.documentElement.style.setProperty("--col03", "#e5ac73");
     hoursDiplay.src = `assets/${season}${hours}.png`;
     minutesDiplay.src = `assets/${season}${minutes}.png`;
     secondsDiplay.src = `assets/${season}${seconds}.png`;
   } else if (month === 6 || month === 7 || month === 8) {
     let season = "winter";
-    document.documentElement.style.setProperty("--col02", "#9ed3f1ff");
+    document.documentElement.style.setProperty("--col01", "#3366cc");
+    document.documentElement.style.setProperty("--col02", "#9ed3f1");
+    document.documentElement.style.setProperty("--col03", "#6c94e6");
     hoursDiplay.src = `assets/${season}${hours}.png`;
     minutesDiplay.src = `assets/${season}${minutes}.png`;
     secondsDiplay.src = `assets/${season}${seconds}.png`;
   } else {
     let season = "spring";
+    document.documentElement.style.setProperty("--col01", "#00916e");
     document.documentElement.style.setProperty("--col02", "#c8ffbe");
+    document.documentElement.style.setProperty("--col03", "#6dd6ac");
     hoursDiplay.src = `assets/${season}${hours}.png`;
     minutesDiplay.src = `assets/${season}${minutes}.png`;
     secondsDiplay.src = `assets/${season}${seconds}.png`;
