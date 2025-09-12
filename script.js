de55fc57e133b1546bcf03e47d89ab301a9f8b38
dashhealy <S4129466@student.rmit.edu.au>
2025-09-12 19:22:47 +1000

    more stuff

--- script.js
+++ script.js
@@ -54,9 +54,3 @@ function timeUI(month, hours, minutes, seconds) {
     secondsDiplay.src = `assets/${season}${seconds}.png`;
   }
 }
-
-//create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
-
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
