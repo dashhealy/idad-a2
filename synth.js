06f1d98cd0f0ba68fbed244e7a3b61dc68d2b068
dashhealy <S4129466@student.rmit.edu.au>
2025-09-13 21:05:46 +1000

    uh

--- synth.js
+++ synth.js
@@ -1,5 +1,18 @@
 //create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+setInterval(checkTime, 1000);
+
+function checkTime() {
+  let currentTimeFull = Date.now();
+  let systemDate = new Date(currentTimeFull);
+
+  let hours = systemDate.getHours();
+  let minutes = systemDate.getMinutes();
+  let seconds = systemDate.getSeconds();
+
+  console.log(hours, minutes, seconds);
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease(`C4`, "8n");
+}
